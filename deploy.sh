39b203030eceefbb35c8bf8ef34296b2a8385ce8
Maroon Ayoub <maroon.ayoub@ibm.com>
2022-04-04 22:24:11 +0300

    permalinks
    Signed-off-by: Maroon Ayoub <maroon.ayoub@ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 echo "using kubeconfig $KUBECONFIG"
 
 # apply service account
-kubectl apply -f serviceaccount.yaml
+kubectl apply -f "https://raw.githubusercontent.com/vMaroon/node-disk-containers-cleaner/main/serviceaccount.yaml"
 # give SA privileged permission (for storage root access)
 oc adm policy add-scc-to-user privileged -z disk-containers-janitor -n default
 # apply cronjob on every node
 for NODE_HOSTNAME in $(kubectl get nodes -l node-role.kubernetes.io/worker='' -o jsonpath='{range .items[*]}{.metadata.labels.kubernetes\.io\/hostname}{"\n"}{end}')
 	do
-		cat cronjob.yaml | sed 's/${NODE_HOSTNAME}/'${NODE_HOSTNAME}'/g' - | kubectl apply -f -
+		curl -s "https://raw.githubusercontent.com/vMaroon/node-disk-containers-cleaner/main/cronjob.yaml" | sed 's/${NODE_HOSTNAME}/'${NODE_HOSTNAME}'/g' - | kubectl apply -f -
 	done
 
